a5f9fd25d601c51c29184b83a537aeead5817fec
azatik <shagabiev_a@mail.ru>
2026-07-21 23:52:53 +0300

    first commit

---
+++ day1/Solution2.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        System.out.println(2025);
+        System.out.println("😅");
+    }
+}
---
+++ day1/Solution3.java
@@ -0,0 +1,7 @@
+package day1;
+
+public class Solution3 {
+    public static void main(String[] args) {
+        
+    }
+}
