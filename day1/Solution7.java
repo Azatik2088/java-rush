fd03292dcfe6e0f65b2b5def3e71bad99e6906c2
azatik <shagabiev_a@mail.ru>
2026-07-22 00:47:45 +0300

    develop solution7

---
+++ day1/Solution7.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Solution7 {
+    public static void main(String[] args) {
+        int currentYear;
+        currentYear = 2025;
+        int projectAge;
+        projectAge = currentYear - 2019;
+        System.out.print(projectAge);
+    }
+}
