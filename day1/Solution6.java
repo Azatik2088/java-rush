b8a53d597524776b36e2abe3c50980fb06f3a01b
azatik <shagabiev_a@mail.ru>
2026-07-22 00:42:58 +0300

    develop solution6

---
+++ day1/Solution6.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class Solution6 {
+    public static void main(String[] args) {
+        String myMessage;
+        myMessage = "I ❤️ YOU";
+        System.out.print(myMessage);
+    }
+}
