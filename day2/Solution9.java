b8576dfcf53b6f2856e50a4fdd46b4e98a1c5287
azatik <shagabiev_a@mail.ru>
2026-07-22 01:04:09 +0300

    develop solution9

---
+++ day2/Solution9.java
@@ -0,0 +1,10 @@
+package day2;
+
+public class Solution9 {
+    public static void main(String[] args) {
+        String userName = "Alice";
+        int userAge = 20;
+        String profileInfo = "Имя: " + userName + ", " + "Возраст: " + userAge;
+        System.out.println(profileInfo);
+    }
+}
