62d38c3c2f81c36280a8d7f3d0dbcca948024989
azatik <shagabiev_a@mail.ru>
2026-07-22 00:30:23 +0300

    develop Solution4

---
+++ day1/Solution4.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Solution4 {
+    public static  void main(String[] args) {
+        System.out.print("Я");
+        System.out.print(" учу");
+        System.out.println(" Java.");
+        System.out.print("Это");
+        System.out.print(" интересно!");
+    }
+}
