9cb1b7e0f467f7bcce8e8bf96a2130e30d681df3
azatik <shagabiev_a@mail.ru>
2026-07-22 00:41:21 +0300

    develop solution5

---
+++ day1/Solution5.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class Solution5 {
+    public static void main(String[] args) {
+        int currentYear;
+        currentYear = 2025;
+        System.out.print(currentYear);
+    }
+}
