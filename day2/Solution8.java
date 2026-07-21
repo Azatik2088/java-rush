223e42924f52599497cb8082c0876a910a3ea4c0
azatik <shagabiev_a@mail.ru>
2026-07-22 00:55:48 +0300

    develop solution8

---
+++ day2/Solution8.java
@@ -0,0 +1,10 @@
+package day2;
+
+public class Solution8 {
+    public static void main(String[] args) {
+        int player1Score = 1999;
+        int player2Score = 25;
+        int totalScore = player1Score + player2Score;
+        System.out.println(totalScore);
+    }
+}
