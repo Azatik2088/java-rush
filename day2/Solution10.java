4d95cd180e631ff742151acda566d177726eb169
azatik <shagabiev_a@mail.ru>
2026-07-22 14:40:46 +0300

    develop comments

---
+++ day2/Solution10.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class Solution10 {
+    public static void main(String[] args) {
+        int itemPrice = 4;
+        int itemCount = 3;
+        int discount = 2;
+        
+        int totalCost = itemPrice * itemCount - discount;
+        String finalMessage = "Итоговая стоимость: " + totalCost;
+        System.out.println(finalMessage);
+    }
+}
---
+++ day2/Solution12.java
@@ -0,0 +1,19 @@
+package day2;
+
+public class Solution12 {
+    public static void main(String[] args)
+    {
+        /* 
+        // Пример объявления переменной типа int
+        int number = 10;
+
+        // Пример объявления переменной типа String
+        String text = "Привет, мир!";
+
+        // Пример вывода переменных на экран
+        System.out.println(number);
+        System.out.println(text);
+        */
+
+    }
+}
