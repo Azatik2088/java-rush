efcab28ad993b3b3e1c6ebb2ca0736ac1c38af56
azatik <shagabiev_a@mail.ru>
2026-07-30 12:12:38 +0300

    сократил прошлый говнокод более коротким говнокодом

--- oop_practise/figures_project/Main.java
+++ oop_practise/figures_project/Main.java
@@ -1,39 +1,40 @@
 package oop_practise.figures_project;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
         List<Figure> figures = new ArrayList<>();
 
+        addFigureSafely(figures, () -> new Circle("Круг", 5));
+        addFigureSafely(figures, () -> new Rectangle("Прямоугольник", 5, 6));
+        addFigureSafely(figures, () -> new Triangle("Треугольник", 5, 6, 7));
+
+        printAnswer(figures);
+    }
+
+
+    private static void addFigureSafely(List<Figure> figures, Supplier<Figure> figureSupplier) {
         try {
-            Circle circle = new Circle("Круг", 5);
-            figures.add(circle);
+            Figure figure = figureSupplier.get();
+            figures.add(figure);
         } catch (IllegalArgumentException e) {
             System.out.println("Ошибка: " + e.getMessage());
         }
-        try {
-            Rectangle circle = new Rectangle("Прямоугольник", 5, 6);
-            figures.add(circle);
-        } catch (IllegalArgumentException e) {
-            System.out.println("Ошибка: " + e.getMessage());
-        }
-        try {
-            Triangle circle = new Triangle("Треугольник", 5, 6, 7);
-            figures.add(circle);
-        } catch (IllegalArgumentException e) {
-            System.out.println("Ошибка: " + e.getMessage());
-        }
+    }
+
+
+    private static void printAnswer(List<Figure> figures) {
         if(!figures.isEmpty()) {
             Collections.sort(figures);
+
             System.out.println("Корректные фигуры");
-            figures.forEach(el -> el.displayInfo());
+            figures.forEach(Figure::displayInfo);
         } else {
             System.out.println("Список фигур пуст, выводить нечего");
         }
-
     }
 }
